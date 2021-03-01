9302061650019ae3ab9ef7637b124cbefefadda6
cti1650 <cti1650@gmail.com>
2021-03-01 12:01:00 +0900

    update PrintA4

--- src/stories/components/PrintArea/A4.tsx
+++ src/stories/components/PrintArea/A4.tsx
@@ -4,8 +4,10 @@ import './A4.css'
 export interface A4Props {}
 
 export const A4: React.FC<A4Props> = ({ ...props }) =>{ return (
+  <div className="page_bg">
     <section className="pageA4" {...props}>
       {props.children}
     </section>
+  </div>
   )
 };
